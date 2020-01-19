70cb35a58bc3b8f2bc16a8bb18058d94e0484eeb
Don Corley <don@donandann.com>
2020-01-19 12:47:09 -0800

    Automate external dependency handling

---
+++ jcalendarbutton/src/main/java/org/sourceforge/jcalendarbutton/JCalendarPopup.java
@@ -0,0 +1,56 @@
+/*
+ * CalendarPopup.java
+ *
+ * Created on March 5, 2000, 5:07 AM
+ * @author Don Corley <don@tourgeek.com>
+ * @version 1.0.0
+ 
+ * Copyright © 2012 jbundle.org. All rights reserved.
+ */
+package org.sourceforge.jcalendarbutton;
+
+import java.util.Date;
+
+/** 
+ * Maintain backwards compatibility when I fixed the package name.
+ */
+@Deprecated
+public class JCalendarPopup extends org.jbundle.util.jcalendarbutton.JCalendarPopup
+{
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Creates new form CalendarPopup.
+     */
+    public JCalendarPopup()
+    {
+        super();
+    }
+    /**
+     * Creates new form CalendarPopup.
+     * @param date The initial date for this button.
+     */
+    public JCalendarPopup(Date date)
+    {
+        super(date);
+    }
+    /**
+     * Creates new form CalendarPopup.
+     * @param strDateParam The name of the date property (defaults to "date").
+     * @param date The initial date for this button.
+     */
+    public JCalendarPopup(String strDateParam, Date date)
+    {
+        super(strDateParam, date);
+    }
+    /**
+     * Creates new form CalendarPopup.
+     * @param strDateParam The name of the date property (defaults to "date").
+     * @param date The initial date for this button.
+     * @param strLanguage The language to use.
+     */
+    public JCalendarPopup(String strDateParam, Date date, String strLanguage)
+    {
+        super(strDateParam, date, strLanguage);
+    }
+}
---
+++ jcalendarbutton/src/main/java/org/sourceforge/jcalendarbutton/JTimePopup.java
@@ -0,0 +1,55 @@
+/*
+ * JTimePopup.java
+ *
+ * @author Don Corley <don@tourgeek.com>
+ * @version 1.0.0
+ 
+ * Copyright © 2012 jbundle.org. All rights reserved.
+ */
+package org.sourceforge.jcalendarbutton;
+
+import java.util.Date;
+
+/** 
+ * Maintain backwards compatibility when I fixed the package name.
+ */
+@Deprecated
+public class JTimePopup extends org.jbundle.util.jcalendarbutton.JTimePopup
+{
+	private static final long serialVersionUID = 1L;
+
+	/**
+     * Creates new form TimePopup.
+     */
+    public JTimePopup()
+    {
+        super();
+    }
+    /**
+     * Creates new form TimePopup.
+     * @param date The initial date for this button.
+     */
+    public JTimePopup(Date date)
+    {
+        super(date);
+    }
+    /**
+     * Creates new form TimePopup.
+     * @param strDateParam The name of the date property (defaults to "date").
+     * @param date The initial date for this button.
+     */
+    public JTimePopup(String strDateParam, Date date)
+    {
+        super(strDateParam, date);
+    }
+    /**
+     * Creates new form TimePopup.
+     * @param strDateParam The name of the date property (defaults to "date").
+     * @param date The initial date for this button.
+     * @param strLanguage The language to use.
+     */
+    public JTimePopup(String strDateParam, Date date, String strLanguage)
+    {
+        super(strDateParam, date, strLanguage);
+    }
+}
